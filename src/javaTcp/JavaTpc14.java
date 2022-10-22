41d02605536fdfc256fd7068d04d3d2483a783be
shwlsdud95 <shwlsdud95@gmail.com>
2022-10-22 16:45:25 +0900

    자바강의 임시저장

---
+++ src/javaTcp/JavaTpc14.java
@@ -0,0 +1,19 @@
+package javaTcp;
+
+import kr.tpc.MemberVO;
+
+public class JavaTpc14 {
+
+	public static void main(String[] args) {	
+		
+	MemberVO m = new MemberVO();
+		
+	m.name = "홍길동";
+	m.age = 18;
+	m.addr = "서울";
+	m.tel = "010-2877-2619"; 
+	
+
+	}
+
+}
---
+++ src/kr/tpc/MemberVO.java
@@ -0,0 +1,13 @@
+package kr.tpc;
+
+public class MemberVO {
+	
+	public String name;
+	public int age;
+	public String tel;
+	public String addr;
+	
+	public MemberVO() {}
+	
+
+}
