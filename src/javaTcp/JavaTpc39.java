be83c78011d9bbdaa7047f479643fcec76bcda97
shwlsdud95 <shwlsdud95@gmail.com>
2023-01-27 17:58:58 +0900

    자바 강의 저장

---
+++ src/javaTcp/JavaTpc39.java
@@ -0,0 +1,49 @@
+package javaTcp;
+
+public class JavaTpc39 {
+
+	public static void main(String[] args) {
+		// wrapper 클래
+		
+		int a = 1;
+		Integer b = new Integer(1);	// Boxing
+		System.out.println(b.intValue());	// 1
+		System.out.println(b.toString());	// "1"
+		
+		Object[] obj = new Object[3];
+		// 1. 2. 3
+		/*
+		obj[0] = new Integer(1);
+		obj[1] = new Integer(2);
+		obj[2] = new Integer(3);
+		*/
+		// 또는
+		// 이 방법은 더욱 진보된 방법
+		obj[0] = 1;
+		obj[1] = 2;
+		obj[2] = 3;
+		
+		System.out.println(obj[0].toString());
+		System.out.println(obj[1].toString());
+		System.out.println(obj[2].toString());
+		
+		// "100" + "100" = 200
+		String x = "100";
+		String y = "100";
+		//System.out.println(x+y);	"100100"
+		
+		// 자바 함수 활용 
+		
+		int v1 = Integer.parseInt(x);
+		int v2 = Integer.parseInt(y);
+		System.out.println(v1+v2); // 200
+		
+		// 100+100="100100"
+		String s1 = String.valueOf(x);
+		String s2 = String.valueOf(y);
+		System.out.println(s1+s2);
+		
+		
+	}
+
+}
